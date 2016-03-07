d9180881e727424b64b92079e5e50b16909c017b
Davy J <dave_jones147@hotmail.com>
2016-03-07 16:20:12 +0100

    Added CodeCraker Analyser

--- ScrapProject/ScrapProject/GlobalSuppressions.cs
+++ ScrapProject/ScrapProject/GlobalSuppressions.cs
@@ -3,3 +3,5 @@
 // scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1024:Use properties where appropriate.", Justification = "<Pending>", Scope = "member", Target = "~M:Westwind.Utilities.Dynamic.SerializableDictionary`2.GetSchema~System.Xml.Schema.XmlSchema")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CC0105:You should use 'var' whenever possible.", Justification = "<Pending>", Scope = "member", Target = "~M:ScrapProject.Program.Encode(System.String)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CC0057:Unused parameters", Justification = "<Pending>", Scope = "member", Target = "~M:ScrapProject.Program.Main(System.String[])")]
